48f6879d69d65aa4dc53e3e1ef9ad7b961392749
giorgi <127527335+nozadze22@users.noreply.github.com>
2024-02-10 16:47:45 +0400

    Add files via upload

---
+++ smoll.js
@@ -0,0 +1,21 @@
+// const info = document.querySelectorAll(".info");
+// const baggs= document.querySelectorAll(".baggs");
+// const img = document.querySelectorAll(".img");
+
+
+// for(let i =0; i<info.length; i++);
+    
+
+// for(let i =0; i< img.length; i++) {
+//     img[i].addEventListener("MouseEnter", () => {
+//         info[i].style = 'opacity:1;'
+//         console.log("hi");
+//     });
+
+//     img[i].addEventListener("MouseLeave", () => {
+//         info[i].style = 'opacity: 0;';
+//     })
+// }
+
+
+
